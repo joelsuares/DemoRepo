348670ba8e45625edb1cf29cc41ed835567e357b
joel <jksuares94@gmail.com>
2021-07-12 14:00:43 +0530

    added property file and test base class

---
+++ src/main/java/com/qa/lbs/testproperty.java
@@ -0,0 +1,12 @@
+package com.qa.lbs;
+
+import org.openqa.selenium.WebDriver;
+
+public class testproperty {
+	
+	WebDriver driver;
+	public testproperty(WebDriver driver) {
+		this.driver=driver;
+	}
+
+}
